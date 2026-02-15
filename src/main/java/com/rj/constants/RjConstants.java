9432085791e27eb7f93c6c25b8e8c9b13ff82daf
renè <rene.menditto@libero.it>
2026-02-15 16:43:41 +0100

    .

--- src/main/java/com/rj/constants/RjConstants.java
+++ src/main/java/com/rj/constants/RjConstants.java
@@ -14,7 +14,7 @@ public class RjConstants {
     public static class RjResponse {
         public static class Status {
             public static final int OK = 200;
-            public static final int NOT_FOUND = 400;
+            public static final int NOT_FOUND = 404;
             public static final int INTERNAL_SERVER_ERROR=500;
         }
     }
