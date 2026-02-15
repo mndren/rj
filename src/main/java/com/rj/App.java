ed4412bdf58683b1f54f0963908b133327119e3b
renè <rene.menditto@libero.it>
2026-02-15 01:50:51 +0100

    -

--- src/main/java/com/rj/App.java
+++ src/main/java/com/rj/App.java
@@ -1,23 +1,47 @@
 package com.rj;
 
+import com.rj.json.Health;
 import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.HttpServerExchange;
+import io.undertow.server.RoutingHandler;
 import io.undertow.util.Headers;
+import org.jboss.logging.Logger;
 
 public class App {
+
+   private static final Logger logger = Logger.getLogger(App.class);
+
     public static void start()
     {
+        logger.info("sto partendo.....");
+
+        RoutingHandler routes = new RoutingHandler()
+                .get("/", exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
+                    exchange.getResponseSender().send("<html lang=\"it\">\n" +
+                            "<head>\n" +
+                            "    <title>RJ</title>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "  <h1>ciao sono rj entra</h1>\n" +
+                            "<hr />\n" +
+                            "<a href=\"/health\">health</a>\n" +
+                            "</body>\n" +
+                            "</html>");
+                })
+
+                .get("/health", exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                    exchange.getResponseSender().send(Health.toJson("ok", "renè", "1,0"));
+                });
+
         Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setHandler(new HttpHandler() {
-                    @Override
-                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
-                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Forza Napoli");
-                    }
-                }).build();
+                .addHttpListener(8080, "0.0.0.0")
+                .setHandler(routes)
+                .build();
+
         server.start();
-        System.out.println("ascolto su http://localhost:8080");
+
+
+        logger.info("ascolto su http://localhost:8080");
     }
 }
---
+++ src/main/java/com/rj/json/Health.java
@@ -0,0 +1,48 @@
+package com.rj.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Health {
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String status;
+    public String name;
+    public String version;
+
+    public static String toJson(String status, String name, String version) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Health h = new Health();
+        h.setStatus(status);
+        h.setName(name);
+        h.setVersion(version);
+
+        return objectMapper.writeValueAsString(h);
+
+    }
+
+}
+
+
